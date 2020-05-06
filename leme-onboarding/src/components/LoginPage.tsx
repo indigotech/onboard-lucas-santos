11531c54149453933f84452b4eff791444058383
lucas-leme <lucaslssantos99@gmail.com>
2020-05-06 17:12:07 -0300

    Add validation functions

--- leme-onboarding/src/components/LoginPage.tsx
+++ leme-onboarding/src/components/LoginPage.tsx
@@ -1,24 +1,78 @@
 import React from 'react';
 import './LoginPage.css';
 
+export interface LoginPageState {
+    email: string;
+    password: string;
+}
+
 export class Welcome extends React.Component{
     render () {
       return<h1>Bem-vindo(a) à Taqtile!</h1> 
     }
   }
 
-export class Login extends React.Component {
+export class Login extends React.Component<{},LoginPageState> {
+
+
+    email: string = "";
+    password: string = "";
+    
+    handleEmail () {
+        this.email = "lucas@taqtile.com"
+    }
+
+    handlePassword () {
+        this.password =  "senha123"
+    }
+
+    validationEmail(email: string) {
+
+        const expression = /^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/;
+
+        if (expression.test(String(email).toLowerCase())) { 
+            return true;
+        }
+        else {
+            return false
+        }
+    }
+
+    validationPassword (password: string) {
+
+        if (password.length < 7) {
+            return false;
+        }
+        else {
+            return true;
+        }
+        
+    }
+
+    private showError = () => {
+        this.handleEmail();
+        this.handlePassword();
+
+        console.log(this.email);
+        console.log(this.validationEmail(this.email));
+
+        console.log(this.password);
+        console.log(this.validationPassword(this.password));
+
+        alert()
+    }
+
     render () {
         return (
            <form>
                 <label>
                         E-mail: 
-                    <input className="App_Form" type="text"/>
+                    <input className="App_Form" type="text" onChange={this.handleEmail}/>
                     <br/>
                         Senha: 
-                    <input className="App_Form" type="password"/>
+                    <input className="App_Form" type="password" onChange={this.handlePassword}/>
                     <br/>
-                    <button className="App_Button">
+                    <button className="App_Button" onClick={this.showError}>
                         Entrar: 
                     </button>
                 </label>
